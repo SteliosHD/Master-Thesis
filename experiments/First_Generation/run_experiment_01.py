5d82577c9a57056808e5a9f968de76c24ed18084
SteliosBitzas <sbitzas@existanze.com>
2022-03-08 19:14:03 +0200

    copy repo from bitbucket

---
+++ experiments/First_Generation/run_experiment_01.py
@@ -0,0 +1,6 @@
+from experiments.First_Generation.experiment import Experiment
+
+first_experiment = Experiment('Official_Experiment')
+first_experiment.run_experiments(result_suffix='_ofc_1_fix', file_suffix='')
+print("Finished")
+print("!"*100)
---
+++ experiments/utils.py
@@ -0,0 +1,50 @@
+from data.utils import config_filepath
+from data.data_file_parser import file_path_parser_from_config, file_pattern_parser
+import json
+
+
+def get_config():
+    with open(config_filepath) as f:
+        config = json.load(f)
+    return config
+
+
+def get_data_file_paths(dataset_keys=None, instance_keys=None, file_suffix='',_project = 'synthetic' ):
+    """
+    Get a list of all the file paths give dataset_keys, instance keys and file suffix
+    :param dataset_keys:
+    :param instance_keys:
+    :param file_suffix:
+    :return: task_paths, worker_paths
+    """
+    config = get_config()
+    project = config['project'][_project]
+    if dataset_keys is None:
+        dataset_keys = project['dataset_keys']
+    file_paths = []
+    tasks_filename = project["tasks_filename"]
+    workers_filename = project["workers_filename"]
+    for dataset_key in dataset_keys:
+        if instance_keys is None:
+            instance_keys = project[dataset_key]
+        for instance_key in instance_keys:
+            file_path = file_path_parser_from_config(dataset_key, instance_key,_project=_project)
+            tasks_file = file_pattern_parser(file_path, tasks_filename, pattern=file_suffix)
+            workers_file = file_pattern_parser(file_path, workers_filename, pattern=file_suffix)
+            file_paths.append((tasks_file, workers_file, (dataset_key,instance_key)))
+        return  file_paths
+
+def get_guru_files(dataset_keys=None, instance_keys=None, file_suffix=''):
+    config = get_config()
+    guru_project = config['project']['guru']
+    tasks_file = guru_project["guru_skill_filepath"]
+    workers_file = guru_project["guru_user_filepath"]
+    return [tasks_file, workers_file]
+
+
+def get_freelancer_files(dataset_keys=None, instance_keys=None, file_suffix=''):
+    config = get_config()
+    freelancer_project = config['project']['freelancer']
+    tasks_file = freelancer_project["freelancer_skill_filepath"]
+    workers_file = freelancer_project["freelancer_user_filepath"]
+    return [tasks_file, workers_file]
